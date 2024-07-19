1795d34e519258b5da78b1a7ae847b19633bf1f6
Falafel098 <fatimaal52006@gmail.com>
2024-07-19 03:54:45 +0200

    something

---
+++ forLoop.c
@@ -0,0 +1,22 @@
+#include <stdio.h>     /*02:50:11*/
+#include <stdlib.h>
+
+/* for loops allows us to use indexing varible - tells us what eteration we curently are on */
+
+
+int main() {
+
+
+	int i = 1; /* i is basicaly telling us how many times we have gone thought the loop */ 
+	while(i <= 5) {
+		printf("%d\n", i);
+		i++;
+	}
+	/* this is what for loop does, in an easir way */ 
+
+	int i;
+	for(i = 1; i <= 5; i++) { /* first section: initializing
+	
+	}
+	return 0;
+}
