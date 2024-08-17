bd5c40bb911f361275bc0529e63c186eea3d27ed
Falafel098 <fatimaal52006@gmail.com>
2024-08-17 03:06:54 +0200

    random string

---
+++ cfiles/rString.c
@@ -0,0 +1,32 @@
+#include <stdio.h> /*used for - printf*/      /* random string */
+#include <stdlib.h> /* used for - rand*/
+#include <time.h> /* used for - time (random string everytime)*/
+
+	static char *rand_string(char *str, size_t size) { /*create randon string and returns*/
+		const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"; /* list of characters that there will be used*/
+		if (size) { /*checks if the size is greater than 0 before making a string*/
+		--size; /*so it knowa when the string ends */
+		for(size_t n = 0; n < size; n++) { /* loop will keep running as long n is less then size*/
+			int key = rand() % (int) (sizeof charset - 1); /* sizeof...-1 would be 62 not 63(removes \0)*/ /* rand()% - ensures that random munber (key) is between 0 - 62*/
+		       	
+	 		str[n] = charset[key]; /* uses key as index to pick a random string*/
+  				}
+      			str[size] = '\0';
+			}
+	return str;
+	}	
+	
+	
+	
+int main() {
+
+	srand(time(NULL)); /*different starting point, each time execute*/
+	size_t size = 10; /*sets random string to 10 characters*/
+	char str[size]; /*empty space for random string*/
+	rand_string(str, size); /*fills empty space with 10 random characters*/
+
+	printf("New code: %s\n", str); /* executes the random string*/
+
+
+	return 0; 
+}
