767f422cd8f0cab9a7a219aad5a9d2bce2880324
Falafel098 <fatimaal52006@gmail.com>
2024-08-29 15:14:38 +0200

    updated randStr

--- cfiles/randStr.c
+++ cfiles/randStr.c
@@ -4,15 +4,19 @@
 
 
 char* rand_string(size_t length) {
+	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	size_t charset_size = sizeof(charset) - 1; /* exclude NULL terminator*/
+	
 	char *str = malloc(length + 1); /*+1 for NULL terminator (\0)*/
 
 	if (!str) { 
 		return NULL; /*handle allocation faliure*/
 	} 
-for (size_t i = 0; i < length; i++) {
-        str[i] = rand() % (122 - 97 + 1) + 97;  /*ASCII table*/
+		for (size_t i = 0; i < length; i++) {
+        	int random_index = rand() % charset_size; /*(122 - 97 + 1) + 97; */
+		str[i]= charset[random_index];
         }
-        str[length] = '\0';
+        	str[length] = '\0';
 
 	return str; /*returns the pointer to the dynamacally allocated random string */
 
